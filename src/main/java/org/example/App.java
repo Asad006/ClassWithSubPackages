da2e0605b3f1f4ab9734f830be286d87901b9a03
meroa006 <merouani.ass@gmail.com>
2021-06-01 12:46:39 -0400

    package works

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,72 +2,44 @@ package org.example;
 
 import java.util.Scanner;
 
+import static org.example.utililies.Mathhelper.*;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
 
+        double result = 0;
         String s1 = getInput("Enter a numeric value: ");
         String s2 = getInput("Enter a numeric value: ");
         String op = getInput("Choose an operation (+ - * /):");
 
-        double result = 0;
-
         try {
             switch (op) {
                 case "+":
-                    result = addValues(s1, s2);
+                    result = addVal(s1, s2);
                     break;
                 case "-":
-                    result = subtractValues(s1, s2);
+                    result = productVal(s1,s2);
                     break;
                 case "*":
-                    result = multiplyValues(s1, s2);
+                    result = subtractionVal(s1,s2);
                     break;
                 case "/":
-                    result = divideValues(s1, s2);
+                    result = divisionVal(s1,s2);
                     break;
                 default:
-                    System.out.println("Unrecognized operation!");
-                    return;
+                    System.out.println("Unrecognized operation");
             }
 
-            System.out.println("The answer is " + result);
-
         } catch (Exception e) {
-            System.out.println("Number formatting exception " + e.getMessage());
+            System.out.println("invalid Numerical value: " + e.getMessage());
         }
+        System.out.println(" Result:"+ result);
     }
 
-    private static String getInput(String prompt) {
-        System.out.print(prompt);
-        Scanner sc = new Scanner(System.in);
-        return sc.nextLine();
-    }
 
-    private static double addValues(String s1, String s2) {
-        double d1 = Double.parseDouble(s1);
-        double d2 = Double.parseDouble(s2);
-        return d1 + d2;
-    }
 
-    private static double subtractValues(String s1, String s2) {
-        double d1 = Double.parseDouble(s1);
-        double d2 = Double.parseDouble(s2);
-        return d1 - d2;
-    }
 
-    private static double multiplyValues(String s1, String s2) {
-        double d1 = Double.parseDouble(s1);
-        double d2 = Double.parseDouble(s2);
-        return d1 * d2;
-    }
-
-    private static double divideValues(String s1, String s2) {
-        double d1 = Double.parseDouble(s1);
-        double d2 = Double.parseDouble(s2);
-        return d1 / d2;
-    }
 }
--- src/main/java/org/example/utililies/Mathhelper.java
+++ src/main/java/org/example/utililies/Mathhelper.java
@@ -1,4 +1,43 @@
 package org.example.utililies;
 
+import java.util.Scanner;
+
 public class Mathhelper {
+
+    public  static double divisionVal(String s1, String s2) {
+        double d1, d2;
+        d1 = Double.parseDouble(s1);
+        d2 = Double.parseDouble(s2);
+        return d1 / d2;
+        // no
+    }
+
+    public static double subtractionVal(String s1, String s2) {
+        double d1, d2;
+        d1 = Double.parseDouble(s1);
+        d2 = Double.parseDouble(s2);
+        return d1 - d2;
+    }
+
+    public static double productVal(String s1, String s2) {
+        double d1, d2;
+        d1 = Double.parseDouble(s1);
+        d2 = Double.parseDouble(s2);
+        return d1 * d2;
+    }
+
+    public static double addVal(String s1, String s2) {
+        double d1, d2;
+        d1 = Double.parseDouble(s1);
+        d2 = Double.parseDouble(s2);
+        return d1 + d2;
+    }
+
+    public static String getInput(String s) {
+        String input;
+        System.out.println(s);
+        Scanner sc = new Scanner(System.in);
+        input = sc.nextLine();
+        return input;
+    }
 }
